14647438b4dbaec28fc5307b2d669b6ecfd48cb0
diptadevroy091-gif <diptadevroy98@gmail.com>
2026-09-13 15:43:31 +0600

    Fix Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/DevStack-A05/",
+  base: process.env.NETLIFY ? "/" : "/DevStack-A05/",
 });
