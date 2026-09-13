13810aaccf534678d311d557fb676aa5f57fef3f
diptadevroy091-gif <diptadevroy98@gmail.com>
2026-09-13 15:46:28 +0600

    Update Done

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NETLIFY ? "/" : "/DevStack-A05/",
+  base: "/DevStack-A05/",
 });
